6679177bf4a25cabf42ee328da321d7d1676b83f
Philipp <pd95@users.noreply.github.com>
2022-12-10 18:02:45 +0100

    Initial Commit

---
+++ TapSudoku/TapSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSudokuApp.swift
+//  TapSudoku
+//
+//  Created by Philipp on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TapSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
